c2d335b7e5d521bb47a01f9c9fe46e154e123cd0
KMishik <k.mishik@gmail.com>
2018-08-03 13:20:01 +0500

    Start BEMTREE. Add outside data in json and tree become function.

--- test.js
+++ test.js
@@ -1,13 +1,37 @@
-/* const fs = require('fs'),
+const fs = require('fs'),
       bemxjst = require('bem-xjst'),
       bemhtml = bemxjst.bemhtml;
 
 let compileTemplate = bemhtml.compile(fs.readFileSync('index.bemhtml.js','utf8'));
- */
 
-let compileTemplate = require("./bundle.bemhtml").BEMHTML;
-let tree = require('./index.bemjson');
+ //let compileTemplate = require("./bundle.bemhtml").BEMHTML;
 
-let html = compileTemplate.apply(tree);
+let data = require('./data.json');
+let tree = function(data) {
+  return {
+    block: 'page',
+    content: data.map(function(user) {
+      return {
+        block: "user",
+        content: [
+          {
+            elem: "name",
+            content: user.user
+          },
+          {
+            elem: "age",
+            content: user.age
+          },
+          {
+            elem: "money",
+            content: user.money
+          }
+        ]
+      }
+    })
+  }
+};
+
+let html = compileTemplate.apply(tree(data));
 
 console.log(html);

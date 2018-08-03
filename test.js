0ed7bbb567e70b66d4df9c688406f67e7ad382d9
KMishik <k.mishik@gmail.com>
2018-08-03 15:42:57 +0500

    Add BEMTREE logic-empty compiled template, and inline markup with data.

--- test.js
+++ test.js
@@ -1,13 +1,17 @@
 const fs = require('fs'),
       bemxjst = require('bem-xjst'),
-      bemhtml = bemxjst.bemhtml;
+      bemhtml = bemxjst.bemhtml,
+      bemtree = bemxjst.bemtree;
 
-let compileTemplate = bemhtml.compile(fs.readFileSync('index.bemhtml.js','utf8'));
+// let compileTemplate = bemhtml.compile(fs.readFileSync('index.bemhtml.js','utf8'));
 
  //let compileTemplate = require("./bundle.bemhtml").BEMHTML;
 
+let compileBemtreeTemplate = bemtree.compile('') ;
+
 let data = require('./data.json');
-let tree = function(data) {
+
+/* let tree = function(data) {
   return {
     block: 'page',
     content: data.map(function(user) {
@@ -30,8 +34,18 @@ let tree = function(data) {
       }
     })
   }
-};
+}; */
 
-let html = compileTemplate.apply(tree(data));
+/* let html = compileTemplate.apply(tree(data));
 
 console.log(html);
+ */
+
+ let bemjson = compileBemtreeTemplate.apply(
+   {
+    block: 'root',
+    data: data
+  }
+);
+
+console.log(bemjson);

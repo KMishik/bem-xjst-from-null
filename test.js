f5bd48e5d8371dda567f69f0542f29d94748a230
KMishik <k.mishik@gmail.com>
2018-08-03 11:48:45 +0500

    Use template from bundle, created using bem-xjst bin util

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
-const fs = require('fs'),
+/* const fs = require('fs'),
       bemxjst = require('bem-xjst'),
       bemhtml = bemxjst.bemhtml;
 
 let compileTemplate = bemhtml.compile(fs.readFileSync('index.bemhtml.js','utf8'));
+ */
 
+let compileTemplate = require("./bundle.bemhtml").BEMHTML;
 let tree = require('./index.bemjson');
 
 let html = compileTemplate.apply(tree);
